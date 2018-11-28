84a8ac8d35819c179cb4b37505b5500f7d996800
Nida Waqar <Nidawaqar@Nidas-MacBook-Pro.local>
2018-11-28 19:39:48 +0300

    new file:   lisp/xah-fly-keys

---
+++ elpa/swiper-20181008.1731/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20181008.1731" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "d76968a85f9dc5dcebdc25eb8e3af2cd2775319e" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
